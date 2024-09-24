a98afb571b82b1081efc8c68c2486dce6d81a226
mohamedelsaid056 <eng.mohamedelsaid1@gmail.com>
2024-09-25 01:32:37 +0300

    first
    first_

---
+++ Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            monthCalendar1 = new MonthCalendar();
+            monthCalendar2 = new MonthCalendar();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            label8 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            label9 = new Label();
+            label10 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label11 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(150, 62);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 28);
+            label1.TabIndex = 0;
+            label1.Text = "From";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(438, 62);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(34, 28);
+            label2.TabIndex = 1;
+            label2.Text = "To";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(471, 62);
+            textBox1.Margin = new Padding(5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(261, 38);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(220, 62);
+            textBox2.Margin = new Padding(5);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(207, 38);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(164, 121);
+            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(196, 25);
+            label3.TabIndex = 5;
+            label3.Text = "choose your schedule";
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(164, 160);
+            monthCalendar1.Margin = new Padding(16, 14, 16, 14);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 6;
+            // 
+            // monthCalendar2
+            // 
+            monthCalendar2.Location = new Point(471, 160);
+            monthCalendar2.Margin = new Padding(16, 14, 16, 14);
+            monthCalendar2.Name = "monthCalendar2";
+            monthCalendar2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(150, 429);
+            label4.Margin = new Padding(5, 0, 5, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(104, 28);
+            label4.TabIndex = 8;
+            label4.Text = "first name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(256, 429);
+            textBox3.Margin = new Padding(5);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(172, 38);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(548, 429);
+            textBox4.Margin = new Padding(5);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(186, 38);
+            textBox4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(438, 429);
+            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 28);
+            label5.TabIndex = 10;
+            label5.Text = "last name";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            label6.Location = new Point(205, 504);
+            label6.Margin = new Padding(5, 0, 5, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(90, 25);
+            label6.TabIndex = 12;
+            label6.Text = "Travel DC";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            radioButton1.Location = new Point(236, 535);
+            radioButton1.Margin = new Padding(5);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(105, 29);
+            radioButton1.TabIndex = 13;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Passport";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            radioButton2.Location = new Point(236, 574);
+            radioButton2.Margin = new Padding(5);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 29);
+            radioButton2.TabIndex = 14;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "ID Card";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            label7.Location = new Point(516, 508);
+            label7.Margin = new Padding(5, 0, 5, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(112, 25);
+            label7.TabIndex = 15;
+            label7.Text = "DC number ";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(516, 550);
+            textBox5.Margin = new Padding(5);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(153, 38);
+            textBox5.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label8.Location = new Point(88, 665);
+            label8.Margin = new Padding(5, 0, 5, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(131, 25);
+            label8.TabIndex = 17;
+            label8.Text = "DC issue date ";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Location = new Point(220, 664);
+            dateTimePicker1.Margin = new Padding(5);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(265, 27);
+            dateTimePicker1.TabIndex = 18;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker2.Location = new Point(634, 665);
+            dateTimePicker2.Margin = new Padding(5);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(265, 27);
+            dateTimePicker2.TabIndex = 20;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label9.Location = new Point(494, 665);
+            label9.Margin = new Padding(5, 0, 5, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(150, 25);
+            label9.TabIndex = 19;
+            label9.Text = "DC expriry date ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(88, 724);
+            label10.Margin = new Padding(5, 0, 5, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(94, 28);
+            label10.TabIndex = 21;
+            label10.Text = "Baggage";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            numericUpDown1.Location = new Point(205, 722);
+            numericUpDown1.Margin = new Padding(5);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(68, 30);
+            numericUpDown1.TabIndex = 22;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(284, 732);
+            label11.Margin = new Padding(5, 0, 5, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(27, 20);
+            label11.TabIndex = 23;
+            label11.Text = "KG";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.MistyRose;
+            button1.Dock = DockStyle.Bottom;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(0, 762);
+            button1.Margin = new Padding(5);
+            button1.Name = "button1";
+            button1.Size = new Size(919, 73);
+            button1.TabIndex = 24;
+            button1.Text = "book";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(919, 835);
+            Controls.Add(button1);
+            Controls.Add(label11);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label10);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(label9);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label8);
+            Controls.Add(textBox5);
+            Controls.Add(label7);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(monthCalendar2);
+            Controls.Add(monthCalendar1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            Margin = new Padding(5);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private MonthCalendar monthCalendar1;
+        private MonthCalendar monthCalendar2;
+        private Label label4;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label5;
+        private Label label6;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label7;
+        private TextBox textBox5;
+        private Label label8;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label9;
+        private Label label10;
+        private NumericUpDown numericUpDown1;
+        private Label label11;
+        private Button button1;
+    }
+}
